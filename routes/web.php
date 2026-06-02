b172e0f8f160f641c5648c7bf1d077c7b4aba35d
THAIDTC <thaidtc@MacBook-Pro-cua-THAIDTC.local>
2026-06-02 18:11:22 +0700

    feat: install model/migrate/controller/route for tasks with cli

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,13 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth'])->name('tasks.')->group(function () {
+    Route::get('/', [TaskController::class, 'index'])->name('index');
+		Route::get('/create', [TaskController::class, 'create'])->name('create');
+		Route::post('/', [TaskController::class, 'store'])->name('store');
+		Route::get('/{task}', [TaskController::class, 'show'])->name('show');
+		Route::get('/{task}/edit', [TaskController::class, 'edit'])->name('edit');
+		Route::put('/{task}', [TaskController::class, 'update'])->name('update');
+		Route::delete('/{task}', [TaskController::class, 'destroy'])->name('destroy');
+});
