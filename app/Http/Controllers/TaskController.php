ac9c2d70392d7972fb1228f1c4d3d9726bd31d6d
THAIDTC <thaidtc@MacBook-Pro-cua-THAIDTC.local>
2026-06-04 11:54:01 +0700

    feat: pagination

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,7 +14,9 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
+        // $tasks = Task::all();
+        // $tasks = Task::latest()->paginate(10);
+        $tasks = Task::with('user')->orderBy('due_date', 'asc')->paginate(10);
         return view('tasks.index', compact('tasks'));
     }
 
